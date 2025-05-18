2faa8f5d9c47effca92518ced3469388664e8252
yuvaramsingh94 <yuvaramsingh94@gmail.com>
2025-05-18 17:31:17 +0530

    attention training model

--- code/Config.py
+++ code/Config.py
@@ -4,10 +4,10 @@ import os
 class Config:
     def __init__(self):
         self.wandb_project = "assignment_3"
-        self.wandb_entity = "v1_basic_full"
+        self.wandb_entity = "v1_attention_full"
         self.epoch = 50
-        self.batch_size = 16
-        self.encoder_embedding_size = 256
+        self.batch_size = 64
+        self.encoder_embedding_size = 512
         self.X_vocab_size = (
             26 + 1
         )  ## Here +1 is because the nn.Embedding layer throws this error AssertionError =  Padding_idx must be within num_embeddings
@@ -22,14 +22,14 @@ class Config:
         ## The actual solution is to use linear layer to adjust the required
         ## shape but it will add additional training parameters
         self.num_encoder_layers = 2
-        self.encoder_dropout_prob = 0.4
+        self.encoder_dropout_prob = 0.2
         self.encoder_bidir = False
         self.encoder_nonlinearity = "tanh"  ## Only for RNN
         self.Y_vocab_size = (
             48 + 1
         )  ## Here +1 is because the nn.Embedding layer throws this error AssertionError =  Padding_idx must be within num_embeddings
         ## Same hidden size mismatch error between encoder and decoder
-        self.decoder_embedding_size = 256
+        self.decoder_embedding_size = 512
         self.Y_padding_idx = 48
         self.decoder_hidden_size = 512
         ## same value as num_encoder_layers
@@ -40,9 +40,9 @@ class Config:
         self.Y_true_vocab_size = (
             48  ## No need for extra digit for padding s required by nn.Embedding
         )
-        self.LR = 0.00004521222677858184
+        self.LR = 0.0008193613448607609
         self.attention_size = 128
-        self.attention_model = False
+        self.attention_model = True
         self.dirpath = os.path.join("weights", "basic")
         if not os.path.exists(self.dirpath):
             os.makedirs(self.dirpath)
